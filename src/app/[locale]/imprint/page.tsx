f9d51996de7459283a39b641cca281158d1cca8b
Max Kolhagen <832938+k6nmx@users.noreply.github.com>
2025-10-29 14:14:09 +0700

    chore(legal): component for language disclaimer, translations and formatting

--- src/app/[locale]/imprint/page.tsx
+++ src/app/[locale]/imprint/page.tsx
@@ -1,55 +1,70 @@
 "use client";
 
-import { useState } from 'react';
-import ContactModal from '@/components/ContactModal';
-//import type {Metadata} from 'next';
-
-// export const metadata: Metadata = {
-//   title: 'Imprint | gobonki',
-//   description: 'Provider identification and contact information.'
-// };
+import { useState } from "react";
+import ContactModal from "@/components/ContactModal";
+import LanguageDisclaimer from "@/components/LanguageDisclaimer";
+import { useLocale, useTranslations } from "next-intl";
 
 export default function ImprintPage() {
-   const [contactModalOpen, setContactModalOpen] = useState(false);
+  const [contactModalOpen, setContactModalOpen] = useState(false);
+
+  const t = useTranslations("legal");
+  const locale = useLocale();
 
   return (
     <>
-        <article className=" bg-gray-50 py-20 px-4">
-      <div className="mx-auto max-w-3xl bg-white p-8 rounded-2xl shadow-md">
-        <h1 className="text-3xl font-semibold text-slate-800 border-b pb-4 mb-6">Imprint</h1>
+      <article className=" bg-gray-50 py-20 px-4">
+        <div className="mx-auto max-w-3xl bg-white p-8 rounded-2xl shadow-md">
+          <h1 className="text-3xl font-semibold text-slate-800 border-b pb-4 mb-6">
+            {t("imprint.title")}
+          </h1>
 
-        <div className="mb-6 p-4 border-l-4 border-yellow-500 bg-yellow-50 text-yellow-800 rounded-md">
-          <h3 className="font-semibold text-lg">Disclaimer</h3>
-          <p className="mt-1">This document is only available in German.</p>
+          {locale !== "de" && <LanguageDisclaimer />}
+
+          <p className="mb-4">
+            <b>runcab GmbH</b>
+            <br />
+            Flottwellstraße 28
+            <br />
+            10785 Berlin
+            <br />
+            Germany
+          </p>
+
+          <h2 className="text-xl font-semibold text-slate-700 mb-2">
+            Kontaktinformationen
+          </h2>
+          <p className="mb-4">
+            <button
+              onClick={() => setContactModalOpen(true)}
+              className="text-blue-600 hover:underline cursor-pointer"
+            >
+              Schick uns eine E-Mail
+            </button>
+          </p>
+
+          <h2 className="text-xl font-semibold text-slate-700 mb-2">
+            Bevollmächtigte Vertreter
+          </h2>
+          <p className="mb-4">
+            Christian Renner
+            <br />
+            Max Kolhagen
+          </p>
+
+          <p className="mb-4">
+            Eingetragener Sitz: Germany
+            <br />
+            Registergericht: Amtsgericht Charlottenburg
+            <br />
+            Handelsregisternummer: HRB 278388 B
+          </p>
         </div>
-
-        <div className="mb-4">runcab GmbH</div>
-        <div className="mb-4">Flottwellstraße 28</div>
-        <div className="mb-4">10785 Berlin</div>
-        <div className="mb-4">Germany</div>
-        
-        <h2 className="text-xl font-semibold text-slate-700 mb-2">Kontaktinformationen:</h2>
-        <p className="mb-4">
-          E-mail:{' '}
-          <button 
-            onClick={() => setContactModalOpen(true)}
-            className="text-blue-600 hover:underline cursor-pointer"
-          >
-            info@gobonki.com
-          </button>
-        </p>
-        
-        <h2 className="text-xl font-semibold text-slate-700 mb-2">Bevollmächtigte Vertreter:</h2>
-        <p className="mb-4">Christian Renner</p>
-        <p className="mb-4">Max Kolhagen</p>
-        
-        <p className="mb-4">Eingetragener Sitz: Germany</p>
-        <p className="mb-4">Registergericht: Amtsgericht Charlottenburg</p>
-        <p className="mb-4">Handelsregisternummer: HRB 278388 B</p>
-      </div>
-    </article>
-    <ContactModal open={contactModalOpen} onOpenChange={setContactModalOpen} />
+      </article>
+      <ContactModal
+        open={contactModalOpen}
+        onOpenChange={setContactModalOpen}
+      />
     </>
-    
   );
 }
